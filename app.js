9db6269607dc31aec7c6a9888ab9d8bf82a71458
Yassine ANZAR BASHA <yassanz.contact@gmail.com>
2024-02-22 21:56:37 +0100

    feat: app.js now returns a JSON response

--- app.js
+++ app.js
@@ -3,5 +3,10 @@ const express = require('express');
 // Create an Express application
 const app = express();
 
+// Return a JSON response object
+app.use((req, res) => {
+    res.json({ message: 'Hello, world' })
+})
+
 // Export the application
 module.exports = app;
