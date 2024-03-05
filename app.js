2a960c3ffd3e4594e5301075574f5a67303b4835
Yassine ANZAR BASHA <yassanz.contact@gmail.com>
2024-03-05 15:01:45 +0100

    feat: GET api/stuff now uses find method

--- app.js
+++ app.js
@@ -46,26 +46,10 @@ app.post('/api/stuff', (req, res, next) => {
 
 // Create a middleware who return stuff information
 app.get('/api/stuff', (req, res, next) => {
-    const stuff = [
-        {
-            _id: 'oeihfzeoi',
-            title: 'Mon premier objet',
-            description: 'Les infos de mon premier objet',
-            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-            price: 4900,
-            userId: 'qsomihvqios',
-        },
-        {
-            _id: 'oeihfzeomoihi',
-            title: 'Mon deuxième objet',
-            description: 'Les infos de mon deuxième objet',
-            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-            price: 2900,
-            userId: 'qsomihvqios',
-        },
-    ];
-    // Return the stuff in JSON format
-    res.status(200).json(stuff);
+    // Return things array with the find method
+    Thing.find()
+        .then(things => res.status(200).json(things))
+        .catch(error => res.status(400).json({ error }));
 })
 
 // Export the application
