eb813aa1d96aac5a4921c9ffecb12bfe82c1977b
Yassine ANZAR BASHA <yassanz.contact@gmail.com>
2024-03-05 11:24:21 +0100

    feat: new middlewares using next

--- app.js
+++ app.js
@@ -3,9 +3,27 @@ const express = require('express');
 // Create an Express application
 const app = express();
 
+// Create a simple middleware
+app.use((req, res, next) => {
+    console.log('Request received !');
+    next();
+});
+
+// Middleware which changes the state of the response
+app.use((req, res, next) => {
+    res.status(201);
+    next();
+})
+
 // Return a JSON response object
+app.use((req, res, next) => {
+    res.json({ message: 'Hello, world' });
+    next();
+});
+
+// Simple middleware
 app.use((req, res) => {
-    res.json({ message: 'Hello, world' })
+    console.log('Response sent !');
 })
 
 // Export the application
