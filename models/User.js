66d37f4e00561a4c67c542a8dee0d601d251f2db
Yassine ANZAR BASHA <yassanz.contact@gmail.com>
2024-03-12 16:58:57 +0100

    feat: new model User with mongoose-unique-validator

---
+++ models/User.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+// import mongoose unique validator
+const uniqueValidator = require('mongoose-unique-validator');
+
+// create a user schema for authentication
+const userSchema = mongoose.schema({
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+});
+
+// use the unique validator plugin to ensure email is unique
+userSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model('User', userSchema);
